5e1866b3fa04a99d79e0a9f94281a4be9fab39c6
ameenmsit <ameennabdulsalam@msitprogram.net>
2021-08-12 06:43:31 +0530

    Solved using Formula nCr, passed all test cases

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -5,8 +5,19 @@
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
 
+#Using nCr=n!/r!(n-r)!
+
 
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	return fact(row)//(fact(col)*fact(row-col))
+
+def fact(n):
+	if(n<=1):
+		return 1
+	else:
+		return n*fact(n-1)
+	
+
+print(fun_pascaltrianglevalue(5,2))
