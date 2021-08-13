1e5a1a8191e16395618a54c045add89a30254cb4
ameenmsit <ameennabdulsalam@msitprogram.net>
2021-08-13 10:01:00 +0530

    Passed 5 testcases

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -7,6 +7,3 @@
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
 	return (1/2*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
-
-# def distance(x1,y1,x2,y2):
-# 	return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
